321fbec0332d149a7270c7f8d2896dd6238aec21
Anggaradm <radmvector@gmail.com>
2022-07-07 15:34:10 +0700

    melanjutkan project airplane

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:bwa2_airplane/shared/theme.dart';
+import 'package:bwa2_airplane/ui/widgets/destination_card.dart';
+import 'package:bwa2_airplane/ui/widgets/destination_tile.dart';
 import 'package:flutter/material.dart';
 import '../../shared/theme.dart';
 
@@ -32,7 +34,7 @@ class HomePage extends StatelessWidget {
                     height: 6,
                   ),
                   Text(
-                    'Where to fly today',
+                    'Where to fly today?',
                     style: greyTextStyle.copyWith(
                       fontSize: 16,
                       fontWeight: light,
@@ -56,9 +58,109 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget popularDestinations() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 30.0,
+        ),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              DestinationCard(
+                name: 'Lake Ciliwung',
+                city: 'Tangerang',
+                imageUrl: 'assets/image_destination1.png',
+                rating: 4.8,
+              ),
+              DestinationCard(
+                name: 'White Houses',
+                city: 'Spain',
+                imageUrl: 'assets/image_destination2.png',
+                rating: 4.7,
+              ),
+              DestinationCard(
+                name: 'Hill heyo',
+                city: 'Monaco',
+                imageUrl: 'assets/image_destination3.png',
+                rating: 4.8,
+              ),
+              DestinationCard(
+                name: 'Menara',
+                city: 'Japan',
+                imageUrl: 'assets/image_destination4.png',
+                rating: 5.0,
+              ),
+              DestinationCard(
+                name: 'Payung Teduh',
+                city: 'Singapore',
+                imageUrl: 'assets/image_destination5.png',
+                rating: 4.8,
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget newDestination() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 30.0,
+          left: defaultMargin,
+          right: defaultMargin,
+          bottom: 100,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'New This Year',
+              style: blackTextStyle.copyWith(
+                fontSize: 18,
+                fontWeight: semiBold,
+              ),
+            ),
+            DestinationTile(
+              name: 'Danau Beratan',
+              city: 'Singaraja',
+              imageUrl: 'assets/image_destination6.png',
+              rating: 4.5,
+            ),
+            DestinationTile(
+              name: 'Sydney Opera',
+              city: 'Australia',
+              imageUrl: 'assets/image_destination7.png',
+              rating: 4.7,
+            ),
+            DestinationTile(
+              name: 'Roma',
+              city: 'Italy',
+              imageUrl: 'assets/image_destination8.png',
+              rating: 4.8,
+            ),
+            DestinationTile(
+              name: 'Payung Teduh',
+              city: 'Singapore',
+              imageUrl: 'assets/image_destination9.png',
+              rating: 4.5,
+            ),
+            DestinationTile(
+              name: 'Hill Hey',
+              city: 'Monaco',
+              imageUrl: 'assets/image_destination10.png',
+              rating: 4.7,
+            ),
+          ],
+        ),
+      );
+    }
+
     return ListView(
       children: [
         header(),
+        popularDestinations(),
+        newDestination(),
       ],
     );
   }
