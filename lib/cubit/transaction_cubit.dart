0580f878f0f863229f5d33fe99d98b31d4fdef9c
Anggaradm <radmvector@gmail.com>
2022-07-20 15:25:03 +0700

    menyelesaikan projek airplane 20-07-22

--- lib/cubit/transaction_cubit.dart
+++ lib/cubit/transaction_cubit.dart
@@ -12,7 +12,19 @@ class TransactionCubit extends Cubit<TransactionState> {
     try {
       emit(TransactionLoading());
       await TransactionService().createTransaction(transaction);
-      emit(TransactionSuccess());
+      emit(TransactionSuccess([]));
+    } catch (e) {
+      emit(TransactionFailed(e.toString()));
+    }
+  }
+
+  void fetchTransactions() async {
+    try {
+      emit(TransactionLoading());
+
+      List<TransactionModel> transaction =
+          await TransactionService().fetchTransactions();
+      emit(TransactionSuccess(transaction));
     } catch (e) {
       emit(TransactionFailed(e.toString()));
     }
